f9940913917ebe97ca5b7a072e917eb8164ab6f2
JhaAyushCanCode <flyingayush2@gmail.com>
2024-06-29 20:47:30 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    env: {
+      RESEND_API_KEY: process.env.RESEND_API_KEY,
+      FROM_EMAIL: process.env.FROM_EMAIL,
+    },
+  };
+  
